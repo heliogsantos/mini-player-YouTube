a8a4de6dd92547940c26da4503a93ff877710a66
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-09-11 14:49:27 -0300

    I insert the animation the button active

--- src/components/IconArrow/Button.js
+++ src/components/IconArrow/Button.js
@@ -5,13 +5,20 @@ import { ArrowIconDown, ArrowIconTop } from '../../utils.js/images.js';
 
 const IconArrow = () => {
   const [icon, setIcon] = React.useState(ArrowIconTop);
+  const [active, setActive] = React.useState(false);
 
   function handleClick() {
     setIcon((icon) => (icon === ArrowIconTop ? ArrowIconDown : ArrowIconTop));
+
+    setActive((active) => (active = true));
+
+    setTimeout(() => {
+      setActive((active) => (active = false));
+    }, 200);
   }
 
   return (
-    <Icon onClick={handleClick} style={{ fill: '#606060' }}>
+    <Icon onMouseDown={handleClick} style={{ fill: '#606060' }} active={active}>
       <path d={icon}></path>
     </Icon>
   );
--- src/components/IconArrow/style.js
+++ src/components/IconArrow/style.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 export const Icon = styled.svg`
-  width: 30px;
-  height: 30px;
+  width: 24px;
+  height: 24px;
   cursor: pointer;
+  transition: 0.1s linear;
+  border-radius: 100%;
+  ${(props) =>
+    props.active &&
+    `
+     background: #ccc;
+    `}
 `;
